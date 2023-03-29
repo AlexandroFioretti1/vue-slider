d6b3d3f70628c9e9d2c8c1ad1ebbd1946736f70f
AlexandroFioretti1 <alexandro.fioretti18@gmail.com>
2023-03-29 14:34:25 +0200

    add html base carousel whit 5 img,

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,14 +6,14 @@ rifare lo slider ma questa volta usando Vue.
 Bonus:
 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
-3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
+3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce.
 */
-
 const { createApp } = Vue;
+
 createApp({
   data() {
     return {
-      title: "Funziona VueJS",
+      message: "Magicamente non funzionerò",
     };
   },
 }).mount("#app");
